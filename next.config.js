e5d5478f97f63225a39bd5b431fa97242af0b800
Luke <luke@rpo-global.com>
2025-03-12 16:19:15 +1300

    feat: add redirect to airtable interface for attendance page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/attendance',
+        destination: 'https://airtable.com/app28ABXN7BKl5b0o/paguL7pjHxfsEPlTD',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-      module.exports = nextConfig
+module.exports = nextConfig
